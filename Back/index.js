38ec13d2d3fd2520881994cdc17b7d9321cef7dd
SamiAbar30 <samiabar30@gmail.com>
2022-12-09 16:27:08 +0100

    final project

---
+++ Back/index.js
@@ -0,0 +1,30 @@
+
+const fs = require('fs');
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors())
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ limit: '25mb' }));
+app.post('/v1/upload-image', (req, res, next) => {
+	try {
+		// to declare some path to store your converted image
+		const path = './images/' + req.body.targetedFileName;
+		const imgdata = req.body.img;
+		// to convert base64 format into random filename
+		const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, '');
+		fs.writeFileSync(path, base64Data, { encoding: 'base64' });
+		return res.status(200).send({ msg: 'your image uploadded with succses' });
+	} catch (e) {
+		return res
+			.status(500)
+			.send({ error: 'there is a problem try to upload an outher one' });
+		next();
+	}
+});
+
+app.listen(3001, () => {
+	console.log(`server running on port 3001`);
+});
--- Front/scirpt.js
+++ Front/scirpt.js
@@ -1,5 +1,44 @@
-$(function(){
-  $("button").click(function(){
-    $("p").hide();
-  });
+$(function () {
+	factChanger();
+
+	$('#upload-image').change(function (e) {
+		handleFile(e);
+	});
 });
+async function factChanger() {
+	let result = await fetch('http://numbersapi.com/1/30/date?json').then(
+		(result) => result.json(),
+	);
+	$('.marquee-fact').text(result.text);
+}
+async function handleFile(e) {
+	let targetedFile = e.target.files[0];
+	let fileExtension = targetedFile.name.split('.').pop();
+	['png', 'jpg', 'jpeg'].forEach((extension) => {
+		if (targetedFile && fileExtension === extension) {
+			let reader = new FileReader();
+			reader.onloadend = async () => {
+				const data = {targetedFileName: targetedFile.name,img: reader.result };
+				const result = await fetch('http://localhost:3001/v1/upload-image', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify(data),
+				}).then((result) => result.json());
+			
+        console.log(result.error);
+        if (result.error) {
+          console.log(result.error);
+          $('#upload-area-text').text(result.error).css({"color":"#ff0000"});
+        }
+        if (result.msg) {
+          console.log(result.msg);
+          $('#upload-area-text').text(result.msg).css({"color":"#4BB543"});
+        }
+      };
+			reader.readAsDataURL(targetedFile);
+      
+		}
+	});
+}
